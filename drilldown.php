ef2014cb75d7e00ac70b18a8ade52863a3626b8e
kimnnnn4215g <kimnnnn4215g@ewha.ac.kr>
2025-11-18 17:56:09 +0900

    Update drilldown.php

--- drilldown.php
+++ drilldown.php
@@ -8,6 +8,18 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 $conn = new mysqli("localhost", "team15", "team15", "team15");
 $conn->set_charset("utf8");
 
+//로그인
+$dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die('DB 연결 실패: ' . $e->getMessage());
+}
+
 // Ajax 호출인지 확인
 $isAjax = isset($_GET['team']) && isset($_GET['metric']);
 if($isAjax){
